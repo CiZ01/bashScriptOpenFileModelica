0b0d9404ff61bf1217d352c476bdc97ac943cdbf
Francesco <33497982+CiZ01@users.noreply.github.com>
2022-10-14 17:08:39 +0200

    Aggiunte opzioni
    Sono state aggiunte l'opzione per aprire i file .mo su VSCode in windows, direttamente da WSL. E un opzione help.

--- openFile.sh
+++ openFile.sh
@@ -6,9 +6,28 @@
 #CONSIGLIO di aggiungerlo alle variabili di ambiente, in modo da richiamare il comando all'interno della cartella interessata
 #+senza dover passare sempre come argomento il path.
 
+help(){
+    printf "Questo script apre in un editor di testo tutti i file .mo presenti nella directory specificata.\n"
+    printf "L'editor di testo predefinito è geany. In caso non sia installato geany userà emacs.\n"
+
+    printf "\n\nOPTIONS\n\n" 
+
+    printf " -c: questa opzione permette di aprire tutti i file in vscode nel caso sia installato. Funziona anche su vscode WSL su Windows.\n Questa opzione potrebbe rallentare l'avvio rispetto editor più semplici\n"
+}
+
 APP=geany
 
-if [ -z "$1" ]
+while getopts ":ch" option; do
+   case $option in
+        c)  APP=code;;
+        h)  help
+            exit;;
+        \?) echo "Opzione non valida!"
+            exit;;
+   esac
+done
+
+if [ -z "$1" ] || [ "$1" == -c ]
 then
     DIR=$PWD
 else
@@ -20,7 +39,9 @@ else
     fi
 fi
 
-if [ $(apt-cache policy geany | grep -c none) == 1 ] 
+echo $APP
+
+if [ $(apt-cache policy geany | grep -c none) == 1 ] && [ ! "$APP" == code ]
 then
     APP=emacs
     echo "Ricordati di chiudere i buffers con il tasto rosso in alto a sinistra!"
@@ -42,8 +63,12 @@ then
     exit 1
 fi
 
-$APP . *.mo & 2> dev/null
+#Su WSL dal momento che non è installato GTK vengono printate dei warning, redirizzo
+#+ questi warning a null per non intasare il terminale. E apro il programma in bg.
+$APP . *.mo 2> /dev/null &
 
 echo "Ho aperto $COUNT files."
+
+echo "$APP"
 
 exit 0
